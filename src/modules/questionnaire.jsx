691ce119bd802435f615863b7c75df8c54317a52
Chesed Kim <starmireu@gmail.com>
2023-12-11 18:18:36 +0900

    refactor : questionnaire 리펙토링 및 chat history 추가중

--- src/modules/questionnaire.jsx
+++ src/modules/questionnaire.jsx
@@ -1,20 +1,18 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { useApi } from './api';
 import { AuthContext } from '../AuthContext';
 import axios from 'axios';
 
 const Questionnaire = () => {
-  const [inputValues, setInputValues] = useState({
-    input1: '',
-    input2: '',
-    input3: '',
-  });
+  const [inputValues, setInputValues] = useState({ input1: '' });
+  const [chatHistory, setChatHistory] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [isIncomplete, setIsIncomplete] = useState(false);
   const { apiPost } = useApi();
   const [chat, setChat] = useState({ prompt: '', response: '' });
-  
+  const { isLoggedIn } = useContext(AuthContext);
+
   const handleInputChange = (e) => {
     const { id, value } = e.target;
     setInputValues((prevValues) => ({
@@ -23,7 +21,24 @@ const Questionnaire = () => {
     }));
   };
 
-  const { isLoggedIn } = useContext(AuthContext);
+  useEffect(() => {
+    const fetchChatHistory = async () => {
+      if (isLoggedIn) {
+        try {
+          const response = await axios.get(`http://localhost/chatbot/history/`, {
+            headers: {
+              "Authorization": `Bearer ${localStorage.getItem('access_token')}`,
+            },
+          });
+          setChatHistory(response.data.chat_history);
+        } catch (error) {
+          console.error('Error fetching chat history:', error);
+        }
+      }
+    };
+
+    fetchChatHistory();
+  }, [isLoggedIn]);
 
   const checkTaskResult = async (task_id) => {
     try {
@@ -36,7 +51,6 @@ const Questionnaire = () => {
       if (result.data && result.data.status !== 'PENDING') {  // Check for a non-PENDING status
         console.log("Received non-PENDING response:", result.data);
         setChat({
-          // prompt: inputValues.input1 + '. ' + inputValues.input2 + '. ' + inputValues.input3 + '.',
           prompt: inputValues.input1,
           response: result.data.result  // Adjusted based on the actual data structure
         });
@@ -52,8 +66,6 @@ const Questionnaire = () => {
     }
   };
 
-  
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsSubmitted(true);
@@ -63,87 +75,73 @@ const Questionnaire = () => {
       return;
     }
 
-    // if (!inputValues.input1 || !inputValues.input2 || !inputValues.input3) {
     if (!inputValues.input1) {
       setIsIncomplete(true);
       return;
     }
 
     setIsIncomplete(false);
-    console.log('Setting isLoading to true');
     setIsLoading(true);
 
     try {
-      // const combinedQuestion = `${inputValues.input1}. ${inputValues.input2}. ${inputValues.input3}.`;
-      const combinedQuestion = `${inputValues.input1}`;
+      const combinedQuestion = inputValues.input1;
       const apiResult = await apiPost(combinedQuestion);
-      console.log("서버로부터 받은 응답:", apiResult);
-      checkTaskResult(apiResult.task_id);  // Start checking for the task result
+      checkTaskResult(apiResult.task_id);
     } catch (err) {
-      console.log("API 호출 중 문제 발생. 확인해주세요.", err);
-    }  // removed the finally block here
+      console.error("API 호출 중 문제 발생. 확인해주세요.", err);
+      setIsLoading(false);
+    }
   };
+
     
 
   return (
-    <form id="questionnaire" onSubmit={handleSubmit}>
-      <div className="threeQ">
-        <h3>대화를 시작해볼까요? 좋아하는 장르, 라이트 노벨 제목 등 추천 받고 싶은 내용을 얘기해보세요.</h3>
-        <input
-          type="text"
-          id="input1"
-          value={inputValues.input1}
-          onChange={handleInputChange}
-          placeholder="어떤 작품에 대해 이야기 해 볼까요?"
-        />
-        {/* <h3>1. 좋아하는 장르가 뭔지 말해주세요.</h3> */}
-        {/* <input
-          type="text"
-          id="input1"
-          value={inputValues.input1}
-          onChange={handleInputChange}
-          placeholder="예) 판타지 장르가 마음에 들었어요. 특히, 현자들의 설전이요!"
-        />
-        <h3>2. 최근에 읽은 라이트 노벨의 제목은 뭔가요?</h3>
-        <input
-          type="text"
-          id="input2"
-          value={inputValues.input2}
-          onChange={handleInputChange}
-          placeholder="없어요 or 늑대와 향신료를 감명깊게 봤어요. 로렌스가 매력이었죠."
-        />
-        <h3>3. 이야기의 분위기는 밝으면 좋겠나요, 어두운 편이 좋나요?</h3>
-        <input
-          type="text"
-          id="input3"
-          value={inputValues.input3}
-          onChange={handleInputChange}
-          placeholder="어느쪽이든요 or 밝은 분위기면 좋겠어요!"
-        /> */}
-        <br />
-      </div>
-      <div className="submitSection">
-        <button type="submit" disabled={isLoading}>
-          검색시작
-        </button>
-        <p>
-          {isLoading
-            ? "잠시만 기다려주세요..."
-            : (isSubmitted && isIncomplete) && "모든 칸의 내용을 채워 주셔야 합니다!"}
-        </p>
-      </div>
-      <div id="loadingSvg" className="loading-svg">
-        {/* SVG 로딩 애니메이션을 여기에 추가 */}
-      </div>
-      {/* 하부에 표시하는 입력값과 API 서버로의 반응내용 */}
+    <>
+      <form id="questionnaire" onSubmit={handleSubmit}>
+        {/* Input field for the user's prompt */}
+        <div className="threeQ">
+          <h3>대화를 시작해볼까요? 좋아하는 장르, 라이트 노벨 제목 등 추천 받고 싶은 내용을 얘기해보세요.</h3>
+          <input
+            type="text"
+            id="input1"
+            value={inputValues.input1}
+            onChange={handleInputChange}
+            placeholder="어떤 작품에 대해 이야기 해 볼까요?"
+          />
+          <br />
+        </div>
+        <div className="submitSection">
+          <button type="submit" disabled={isLoading}>검색시작</button>
+          <p>
+            {isLoading ? "잠시만 기다려주세요..." : isSubmitted && isIncomplete && "내용을 입력해주세요!"}
+          </p>
+        </div>
+      </form>
+
+      {/* Display for current chat prompt and response */}
       <div className="wrapperAnswer">
-        <ul>
-          {chat.prompt && <li className="prompt">당신의 키워드: {chat.prompt}</li>}
-          {chat.response && <li className="response">답변: {chat.response}</li>}
-        </ul>
+        {chat.prompt && <p>당신의 키워드: {chat.prompt}</p>}
+        {chat.response && <p>답변: {chat.response}</p>}
       </div>
-    </form>
-  );  
+
+      {/* Display for chat history */}
+      <div className="chat-history">
+        <h2>Chat History:</h2>
+        {chatHistory.length > 0 ? (
+          <ul>
+            {chatHistory.map((historyItem, index) => (
+              <li key={index}>
+                <div><strong>Prompt:</strong> {historyItem.prompt}</div>
+                <div><strong>Response:</strong> {historyItem.response}</div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No chat history to display.</p>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Questionnaire;
